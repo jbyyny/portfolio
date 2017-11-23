a1e568c6f5875dbe3d845d1bb95200fec6c8bd47
jenbyyny <jbyyny@users.noreply.github.com>
2017-11-23 12:13:49 -0700

    correct changes from class

--- js/main.js
+++ js/main.js
@@ -21,3 +21,24 @@ for(var i=0; i<bodyElements.length; ++i) {
 hamburger.addEventListener('click', function () {
     toggleNav();
 });
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("projects-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
