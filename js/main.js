ba4783846fdaf4f344bbd234e4e1fb6b23c8cab9
jenbyyny <jbyyny@users.noreply.github.com>
2017-12-01 20:14:03 -0700

    updates and fixes from Fred
    just about done. need to link a few images from index to project pages

--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
-var nav = document.getElementById('nav');
-var hamburger = document.getElementById('hamburger');
+$(document).ready(function(){
 
-function toggleNav() {
-    if (nav.className === 'open') {
-        nav.className = '';
-    } else {
-        nav.className = 'open';
-    }
-}
+  // non-mobile dropdown
+  $("#nav .dropbtn").click(function(){
+    $("#myDropdown").slideToggle();
+  });
 
-var bodyElements = document.getElementsByClassName('body');
-for(var i=0; i<bodyElements.length; ++i) {
-    bodyElements[i].addEventListener('click', function (e) {
-        if (nav.className === 'open') {
-            toggleNav();
-        }
-    });
-};
+  // for mobile dropdown
+  $("#nav-mobile .dropbtn").click(function(){
+    $("#nav-mobile li hidden").slideToggle();
+  });
 
-hamburger.addEventListener('click', function () {
-    toggleNav();
-});
-
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("projects-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+  // hamburger functionality
+  $("#hamburger").click(function(){
+    var nav = $('#nav-mobile');
+      if(nav.attr('class') === 'open') {
+          nav.attr('class', '');
+      } else {
+          nav.attr('class', 'open');
       }
-    }
-  }
-}
+  });
+
+  // var nav = document.getElementById('nav');
+  // var hamburger = document.getElementById('hamburger');
+
+
+  // var bodyElements = document.getElementsByClassName('body');
+  // for(var i=0; i<bodyElements.length; ++i) {
+  //     bodyElements[i].addEventListener('click', function (e) {
+  //         if (nav.className === 'open') {
+  //             toggleNav();
+  //         }
+  //     });
+  // };
+
+
+  /* When the user clicks on the button, 
+  toggle between hiding and showing the dropdown content */
+  // function myFunction(event) {
+  //   if(event){event.preventDefault();}
+  //   document.getElementById("myDropdown").classList.toggle("show");
+  // }
+
+  // Close the dropdown if the user clicks outside of it
+  // window.onclick = function(event) {
+  //   if (!event.target.matches('.dropbtn')) {
+
+  //     var dropdowns = document.getElementsByClassName("projects-content");
+  //     var i;
+  //     for (i = 0; i < dropdowns.length; i++) {
+  //       var openDropdown = dropdowns[i];
+  //       if (openDropdown.classList.contains('show')) {
+  //         openDropdown.classList.remove('show');
+  //       }
+  //     }
+  //   }
+  // }
+
+});
