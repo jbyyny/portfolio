53ca4799f526ad84628024ff2e34f8ddc00e67b4
jenbyyny <jbyyny@users.noreply.github.com>
2017-12-05 10:20:45 -0700

    updates to dropdown, thanks Fred!

--- js/main.js
+++ js/main.js
@@ -7,53 +7,25 @@ $(document).ready(function(){
 
   // for mobile dropdown
   $("#nav-mobile .dropbtn").click(function(){
-    $("#nav-mobile li hidden").slideToggle();
+    // $("#nav-mobile li hidden").slideToggle();
   });
 
   // hamburger functionality
   $("#hamburger").click(function(){
     var nav = $('#nav-mobile');
-      if(nav.attr('class') === 'open') {
-          nav.attr('class', '');
-      } else {
-          nav.attr('class', 'open');
-      }
+      // if(nav.attr('class') === 'open') {
+      //     nav.attr('class', '');
+      // } else {
+      //     nav.attr('class', 'open');
+      // }
+      $('#nav-mobile').slideToggle();
   });
 
-  // var nav = document.getElementById('nav');
-  // var hamburger = document.getElementById('hamburger');
 
-
-  // var bodyElements = document.getElementsByClassName('body');
-  // for(var i=0; i<bodyElements.length; ++i) {
-  //     bodyElements[i].addEventListener('click', function (e) {
-  //         if (nav.className === 'open') {
-  //             toggleNav();
-  //         }
-  //     });
-  // };
-
-
-  /* When the user clicks on the button, 
-  toggle between hiding and showing the dropdown content */
-  // function myFunction(event) {
-  //   if(event){event.preventDefault();}
-  //   document.getElementById("myDropdown").classList.toggle("show");
-  // }
-
-  // Close the dropdown if the user clicks outside of it
-  // window.onclick = function(event) {
-  //   if (!event.target.matches('.dropbtn')) {
-
-  //     var dropdowns = document.getElementsByClassName("projects-content");
-  //     var i;
-  //     for (i = 0; i < dropdowns.length; i++) {
-  //       var openDropdown = dropdowns[i];
-  //       if (openDropdown.classList.contains('show')) {
-  //         openDropdown.classList.remove('show');
-  //       }
-  //     }
-  //   }
-  // }
-
+  if( $(window).width() > 999 ){
+    $("#nav-mobile").addClass('hide');
+  } else {
+    $("#nav-mobile").removeClass('hide');
+  }
+  
 });
